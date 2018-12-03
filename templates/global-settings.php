9018a6a25399693f005db9b1faca92f5851f36cb
Kyle Scheuing <oppzippy@gmail.com>
2018-12-03 12:37:37 -0500

    Rename locale keys to be consistent with file names

--- templates/global-settings.php
+++ templates/global-settings.php
@@ -17,7 +17,7 @@
 		<label for="map-select"><?= $locale->get('map') ?></label>
 		<select id="map-select">
 			<?php for ($i = 1; $i <= 10; $i++): ?>
-				<option value="uldir_<?= $i ?>"><?= $locale->get("Uldir_$i") ?></option>
+				<option value="uldir_<?= $i ?>"><?= $locale->get("uldir_$i") ?></option>
 			<?php endfor; ?>
 		</select>
 	</div>
